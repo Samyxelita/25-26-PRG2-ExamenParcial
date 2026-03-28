d61406e16b15af895557ebd51bb7a99abd77634c
samyx <samyxelita@gmail.com>
2026-03-28 19:20:59 +0100

    feat: implementar atributos y constructor de Partido

--- entregas/herreraSamuel/src/Partido.java
+++ entregas/herreraSamuel/src/Partido.java
@@ -1,5 +1,23 @@
 public class Partido {
-    public Partido(Equipo local, Equipo visitante, Fecha fecha)
+    private Fecha fecha;
+    private Equipo equipoLocal;
+    private Equipo equipoVisitante;
+    private int golesLocal;
+    private int golesVisitante;
+    private boolean estaJugado;
+
+    public Partido(Equipo local, Equipo visitante, Fecha fecha) {
+        assert local != visitante; 
+        
+        this.fecha = fecha;
+        this.equipoLocal = local;
+        this.equipoVisitante = visitante;
+        
+        this.golesLocal = 0;
+        this.golesVisitante = 0;
+        this.estaJugado = false;
+    } 
+
     public void registrarResultado(int golesLocal, int golesVisitante)
     public boolean estaJugado()
     public void mostrar()
