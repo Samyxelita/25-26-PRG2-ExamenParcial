e4fb42c7e611420ac5772737cc02ddfc56f74164
samyx <samyxelita@gmail.com>
2026-03-28 18:46:11 +0100

    feat: crear clase Fecha con atributos y constructor validado

---
+++ entregas/herreraSamuel/src/Fecha .java
@@ -0,0 +1,20 @@
+class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha(int dia, int mes, int anio){
+        assert dia >= 1 && dia <= 31;
+        assert mes >= 1 && mes <= 12;
+        assert anio > 0;
+        
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public boolean esIgual(Fecha otraFecha){
+        return false;
+    }
+    public void mostrar(){}
+}
