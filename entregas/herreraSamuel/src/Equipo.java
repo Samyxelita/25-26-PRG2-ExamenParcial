fdd4f8035259a9f3007f6f25c287cf05f7f1f9ae
samyx <samyxelita@gmail.com>
2026-03-28 18:06:12 +0100

    feat: definir la vista pública de la clase Equipo

---
+++ entregas/herreraSamuel/src/Equipo.java
@@ -0,0 +1,14 @@
+public class Equipo {
+    
+    public Equipo(String nombre, String color, int cantidadJugadores) {}
+    
+    public String nombre() { return ""; } 
+    
+    public String color() { return ""; }
+    
+    public int puntos() { return 0; }
+    
+    public void sumarPuntos(int puntos) {} 
+    
+    public void mostrar() {}
+}
