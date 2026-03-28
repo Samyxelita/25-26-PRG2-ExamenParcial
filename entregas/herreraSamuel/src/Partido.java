aac4a26a2fb88ad910bef028d558168d34918616
samyx <samyxelita@gmail.com>
2026-03-28 19:01:34 +0100

    feat: definir la vista publica de la clase Partido

---
+++ entregas/herreraSamuel/src/Partido.java
@@ -0,0 +1,7 @@
+public class Partido {
+    public Partido(Equipo local, Equipo visitante, Fecha fecha)
+    public void registrarResultado(int golesLocal, int golesVisitante)
+    public boolean estaJugado()
+    public void mostrar()
+}
+
