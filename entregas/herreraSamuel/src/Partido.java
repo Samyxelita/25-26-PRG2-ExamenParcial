c254035430901c6e3929fad6e7c75bf3e8614e2a
samyx <samyxelita@gmail.com>
2026-03-28 19:26:23 +0100

    feat: metodos de la clase Partido

--- entregas/herreraSamuel/src/Partido.java
+++ entregas/herreraSamuel/src/Partido.java
@@ -18,8 +18,37 @@ public class Partido {
         this.estaJugado = false;
     } 
 
-    public void registrarResultado(int golesLocal, int golesVisitante)
-    public boolean estaJugado()
-    public void mostrar()
+    public void registrarResultado(int golesLocal, int golesVisitante){
+        assert golesLocal >= 0 && golesVisitante >= 0;
+        assert !this.estaJugado; 
+
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+        this.estaJugado = true;
+
+        if (this.golesLocal > this.golesVisitante) {
+            this.equipoLocal.sumarPuntos(3);
+        } else if (this.golesLocal < this.golesVisitante) {
+            this.equipoVisitante.sumarPuntos(3);
+        } else {
+            this.equipoLocal.sumarPuntos(1);
+            this.equipoVisitante.sumarPuntos(1);
+        }
+    }
+
+    public boolean estaJugado(){
+        return this.estaJugado;
+    }
+    public void mostrar(){
+        if (this.estaJugado) {
+            System.out.println("Resultado: " + this.equipoLocal.nombre() + " " + 
+                               this.golesLocal + " - " + this.golesVisitante + 
+                               " " + this.equipoVisitante.nombre());
+        } else {
+            System.out.print("Partido pendiente para la fecha: ");
+            this.fecha.mostrar(); 
+            System.out.println(this.equipoLocal.nombre() + " vs " + this.equipoVisitante.nombre());
+        }
+    }
 }
 
